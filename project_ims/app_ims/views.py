6037a8828573d9e4f276a864aa3221ea458ecdb5
jonathan144000 <elohistnepal@gmail.com>
2022-12-20 11:52:24 +0545

    create, index, show & edit page created with view & urls

--- project_ims/app_ims/views.py
+++ project_ims/app_ims/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def item_index(request):
+    return render(request, 'items/index.html')
